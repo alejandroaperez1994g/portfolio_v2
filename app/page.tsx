67ee55aa07b44d66b568f7774a80260be62341be
Alejandro <alejandroaperez1994g@gmail.com>
2023-08-23 10:50:44 +0200

    :recycle: added missing sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
-import Profile from "@/views/sections/Profile/Profile";
 import Hero from "@/views/sections/Hero/Hero";
 import About from "@/views/sections/About/About";
 import Resume from "@/views/sections/Resume/Resume";
+import Skills from "@/views/sections/Skills/Skills";
+import Projects from "@/views/sections/Projects/Projects";
+import Contact from "@/views/sections/Contact/Contact";
+import Profile from "@/views/sections/Profile/Profile";
 
 export default function Home() {
     return (
-        <main className="flex flex-col items-center gap-4 bg-black pt-6 align-middle">
-            <Profile/>
-            <Hero/>
-            <About/>
-            <Resume/>
+        <main className="flex min-w-full lg:justify-center">
+            <div
+                className="flex lg:max-w-[770px]  flex-col items-center gap-14 lg:gap-36 pt-6 align-middle bg-[#1f1f1f] ">
+                <Profile/>
+                <Hero/>
+                <About/>
+                <Resume/>
+                <Skills/>
+                <Projects/>
+                <Contact/>
+            </div>
         </main>
     )
 }
