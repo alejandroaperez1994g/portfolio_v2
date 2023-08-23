d857577f79a64e65a670bd63b08c8ca53ae7bfa6
Alejandro <alejandroaperez1994g@gmail.com>
2023-08-23 10:32:18 +0200

    :art: added responsiveness for large screens

--- views/commons/heading/Heading.tsx
+++ views/commons/heading/Heading.tsx
@@ -1,4 +1,4 @@
-import {JSX} from "react";
+import {JSX} from 'react';
 
 type Props = {
     text: string;
@@ -6,7 +6,7 @@ type Props = {
 };
 const Heading = ({text, icon}: Props) => {
     return (
-        <h5 className="mb-6 flex items-center gap-2 self-start rounded-3xl border border-gray-500 px-3 opacity-100 py-1.5 text-[0.8rem] ">
+        <h5 className="mb-6 lg:mb-8 self-start flex items-center gap-2  rounded-3xl border border-gray-500 px-3 opacity-100 py-1.5 text-[0.8rem] ">
             {icon}
             {text}
         </h5>
