36a6c84effca56b933f24201cfb2683de258233c
Alejandro <alejandroaperez1994g@gmail.com>
2023-08-23 10:15:21 +0200

    :sparkles: added compatibility with fontawesome icons

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,9 +1,10 @@
 import {config} from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import type {AppProps} from "next/app";
+import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
 
 config.autoAddCss = false
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({Component, pageProps}: AppProps) {
     return <Component {...pageProps} />
 }
