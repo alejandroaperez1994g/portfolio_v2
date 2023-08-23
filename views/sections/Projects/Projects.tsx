b1465953c367b6e65f8572ce8a0db4d7b2f5ff15
Alejandro <alejandroaperez1994g@gmail.com>
2023-08-23 10:24:35 +0200

    :sparkles: added projects section with mock data

---
+++ views/sections/Projects/Projects.tsx
@@ -0,0 +1,23 @@
+import Heading from "@/views/commons/heading/Heading";
+import projects from "@/views/sections/Projects/data";
+import ProjectCard from "@/views/commons/project_card/ProjectCard";
+import cubes from "@/public/assets/icons/cubes.svg";
+import Image from "next/image";
+
+const Projects = () => {
+    return (
+        <section id={"portfolio"} className="flex flex-col rounded-3xl border-slate-500 p-4 w-[98%]">
+            <Heading text={"PORTFOLIO"} icon={<Image priority src={cubes} alt="cubesIcon"/>}/>
+            <h1 className="mb-10 text-4xl">Featured <span className="text-[#27e98b]">Projects</span></h1>
+            <div className="flex flex-col lg:flex-wrap lg:flex-row lg:justify-around gap-8">
+                {
+                    projects.map(({title, technologies, link, image}, index) => (
+                        <ProjectCard key={index} title={title} image={image} link={link} technologies={technologies}
+                        />
+                    ))
+                }
+            </div>
+        </section>
+    );
+};
+export default Projects;
---
+++ views/sections/Projects/data.ts
@@ -0,0 +1,39 @@
+const projects = [
+    {
+        title: 'Project 1',
+        image: 'https://www.wpriverthemes.com/HTML/drake/assets/images/portfolio1.jpg',
+        link: "https://google.com",
+        type: "large",
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+        title: 'Project 2',
+        image: 'https://www.wpriverthemes.com/HTML/drake/assets/images/portfolio1.jpg',
+        link: "https://google.com",
+        type: "small",
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+        title: 'Project 3',
+        image: 'https://www.wpriverthemes.com/HTML/drake/assets/images/portfolio1.jpg',
+        link: "https://google.com",
+        type: "small",
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+        title: 'Project 4',
+        image: 'https://www.wpriverthemes.com/HTML/drake/assets/images/portfolio1.jpg',
+        link: "https://google.com",
+        type: "large",
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+    }, {
+        title: 'Project 5',
+        image: 'https://www.wpriverthemes.com/HTML/drake/assets/images/portfolio1.jpg',
+        link: "https://google.com",
+        type: "small",
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+    }
+]
+
+
+export default projects;
