f22f80543e9131bda85e41ae4707919b03252f4b
Alejandro <alejandroaperez1994g@gmail.com>
2023-08-23 10:26:52 +0200

    :recycle: replaced fontawesome icons by svg

--- views/sections/About/About.tsx
+++ views/sections/About/About.tsx
@@ -1,20 +1,27 @@
-import Heading from "@/views/commons/heading/Heading";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUser} from "@fortawesome/free-solid-svg-icons";
+import Heading from '@/views/commons/heading/Heading';
+import userIcon from '@/public/assets/icons/user.svg';
+import Image from 'next/image';
 
 const About = () => {
     return (
-        <section className="flex flex-col items-center rounded-3xl border-slate-500 p-4 w-[98%] h-[60vh]">
-            <Heading text={"ABOUT"} icon={<FontAwesomeIcon icon={faUser}/>}/>
+        <section className="flex flex-col items-center rounded-3xl border-slate-500 p-4 w-[98%] ">
+            <Heading
+                text={'ABOUT'}
+                icon={<Image priority src={userIcon} alt="userIcon"/>}
+            />
             <div>
                 <h1 className="text-4xl mb-10 ">
-                    Every great design begin with an even <span className="text-[#27e98b]">better story</span>
+                    Every great design begin with an even{' '}
+                    <span className="text-[#27e98b]">better story</span>
                 </h1>
-                <p>
-                    Since beginning my journey as a freelance designer nearly 8 years ago, I've done remote work for
-                    agencies, consulted for startups, and collaborated with talented people to create digital products
-                    for both business and consumer use. I'm quietly confident, naturally curious, and perpetually
-                    working on improving my chopsone design problem at a time.
+                <p className="text-gray-500 lg:leading-8 lg:max-w-xl ">
+                    I'm an Informatics Engineer with 5 years of experience in web development. I've worked with
+                    companies like Desoft, delivering end-to-end systems and apps. My passion for tech keeps me learning
+                    and updated on industry best practices. With a Master's in Software Development from Barcelona, I'm
+                    skilled in HTML, CSS, JS, React, Node.js, Laravel, Express.js, MySQL, PostgreSQL, MongoDB, and more,
+                    with expertise in REST APIs, and tools like Jest and Git. I've contributed to impactful projects,
+                    like Miner application, which expanded the company's offerings and portfolio. I'm a dedicated
+                    Full-stack Developer ready to bring expertise to your team.
                 </p>
             </div>
         </section>
