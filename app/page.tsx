b3227e73c5c3fa12c0fb6889f0ec9b20127eeb98
Alejandro <alejandroaperez1994g@gmail.com>
2023-08-25 13:16:43 +0200

    :recycle: added property to know component location

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Hero from "@/views/sections/Hero/Hero";
 import About from "@/views/sections/About/About";
 import Resume from "@/views/sections/Resume/Resume";
@@ -8,10 +9,10 @@ import Profile from "@/views/sections/Profile/Profile";
 
 export default function Home() {
     return (
-        <main className="flex min-w-full lg:justify-center">
+        <main className="flex m-auto lg:justify-center">
             <div
                 className="flex lg:max-w-[770px]  flex-col items-center gap-14 lg:gap-36 pt-6 align-middle bg-[#1f1f1f] ">
-                <Profile/>
+                <Profile contained={true}/>
                 <Hero/>
                 <About/>
                 <Resume/>
