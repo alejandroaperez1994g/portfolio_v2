618328db21770400c17023bcc96958cb043e03a9
Alejandro <alejandroaperez1994g@gmail.com>
2023-08-25 14:13:53 +0200

    :recycle: implemented loading view on first load

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,24 @@ import Skills from "@/views/sections/Skills/Skills";
 import Projects from "@/views/sections/Projects/Projects";
 import Contact from "@/views/sections/Contact/Contact";
 import Profile from "@/views/sections/Profile/Profile";
+import {useEffect, useState} from "react";
+import Loading from "@/app/loading";
 
 export default function Home() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulación de una solicitud de red o tarea que lleva tiempo
+        setTimeout(() => {
+            setIsLoading(false); // Cuando la tarea se completa, establece isLoading en false
+        }, 2000); // Esto simula una espera de 2 segundos
+    }, []);
+
+    if (isLoading) {
+        return <Loading/>;
+    }
+
+
     return (
         <main className="flex m-auto lg:justify-center">
             <div
