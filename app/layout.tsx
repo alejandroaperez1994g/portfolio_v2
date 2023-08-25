c59864891765c214efbaac394d223a2d45b36696
Alejandro <alejandroaperez1994g@gmail.com>
2023-08-25 12:33:44 +0200

    :recycle: added default layout for desktop view

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import {ReactNode} from "react";
+import Profile from "@/views/sections/Profile/Profile";
+import VerticalNavigator from "@/views/sections/VerticalNavigator/VerticalNavigator";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -10,14 +12,16 @@ export const metadata: Metadata = {
     description: 'Alejandro Avila - Portfolio',
 }
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: ReactNode
-}) {
+export default function RootLayout({children,}: { children: ReactNode }) {
     return (
         <html lang="en">
-        <body className={`${inter.className} text-white min-h-screen flex`}>{children}</body>
+        <body className={`${inter.className} text-white min-h-screen flex `}>
+        <div className="pl-2 fixed top-0 left-0 z-10 flex flex-shrink-0 items-center align-middle">
+            <Profile/>
+        </div>
+        {children}
+        <div className="fixed top-0 right-0 z-10 flex-shrink-0 items-center align-middle"><VerticalNavigator/></div>
+        </body>
         </html>
     )
 }
