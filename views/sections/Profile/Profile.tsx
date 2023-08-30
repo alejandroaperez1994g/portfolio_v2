dd8203c1afa9e80ba52c1d7f6fbd309b32b2ec46
Alejandro <alejandroaperez1994g@gmail.com>
2023-08-31 01:28:34 +0200

    :recycle: updated profile picture

--- views/sections/Profile/Profile.tsx
+++ views/sections/Profile/Profile.tsx
@@ -14,7 +14,7 @@ export default function Profile({contained}: IProfileProps) {
                  className={`${contained ? "lg:hidden" : "hidden 2xl:flex max-h-screen mt-[40%]"} mb-14 flex flex-1 flex-col items-center rounded-3xl border border-slate-500 p-4 w-[98%] h-[90%]`}>
             <p className={`${!contained && "pl-[30%] "}mb-10 self-end pb-6`}>Software Developer</p>
             <Image
-                src={"https://cdnb.artstation.com/p/assets/images/images/002/322/823/large/hicham-habchi-cover2.jpg?1460289817"}
+                src={"https://res.cloudinary.com/ddftiga06/image/upload/v1693437818/portfolio/profile-picture/6186_wv5aay.jpg"}
                 alt="Profile Picture" width={200} height={200}
                 className={`rounded-3xl mb-[4rem]`}/>
             <h2 className={`text-xl`}>alejandroaperez1994g@gmail.com</h2>
