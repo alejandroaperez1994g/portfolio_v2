48d14089593ff943984329204c312174765c3a2c
Alejandro <alejandroaperez1994g@gmail.com>
2023-08-31 01:28:24 +0200

    :recycle: updated props uses

--- views/commons/project_card/ProjectCard.tsx
+++ views/commons/project_card/ProjectCard.tsx
@@ -1,24 +1,28 @@
-type IProjectCardProps = {
+import Link from "next/link";
+
+export interface IProjectCardProps {
     title: string,
     image: string,
     link: string,
     technologies: string[],
-};
+}
+
 const ProjectCard = ({title, image, link, technologies}: IProjectCardProps) => {
     return (
         <article className="mb-6 w-full lg:w-[47%] group">
             <div
-                className={`flex border items-end p-3 border-slate-500 rounded-3xl w-full  bg-cover bg-no-repeat bg-center h-[370px] mb-3`}
-                style={{backgroundImage: "url(https://www.wpriverthemes.com/HTML/drake/assets/images/portfolio1.jpg)"}}>
+                className={`flex border flex-wrap content-end items-end p-3 border-slate-500 rounded-3xl w-full  bg-cover bg-no-repeat bg-center h-[370px] mb-3`}
+                style={{backgroundImage: `url(${image})`}}>
                 {
                     technologies.map((technology, index) => (
                         <span key={index}
-                              className="h-max mr-2 rounded-full bg-white px-4 py-2 text-xs text-black transition ease-in-out duration-400 group-hover:bg-[#1f1f1f] group-hover:text-white">{technology}</span>
+                              className="h-max mr-2 rounded-full bg-white mb-1 px-4 py-2 text-xs text-black transition ease-in-out duration-400 group-hover:bg-[#1f1f1f] group-hover:text-white">{technology}</span>
                     ))
                 }
             </div>
-            <h4 className="text-2xl transition ease-in-out duration-600 group-hover:underline">{title}</h4>
-
+            <Link href={link} target="_blank">
+                <h4 className="text-2xl transition ease-in-out duration-600 group-hover:underline">{title}</h4>
+            </Link>
 
         </article>
     );
